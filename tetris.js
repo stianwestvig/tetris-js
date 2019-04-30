75eaa4a5a59e82d2eb43753acc8ce1fb1f223b99
Stian Westvig <stian.westvig@bouvet.no>
2019-04-30 20:44:12 +0200

    Add better game over handling and external css

--- tetris.js
+++ tetris.js
@@ -1,38 +1,42 @@
-const canvas = document.querySelector('#tetris')
-const scoreElement = document.querySelector('#score')
-const ctx = canvas.getContext('2d')
-
 const BOARD_ROWS = 20
 const BOARD_COLUMNS = 10
-const SQUARE = SQUAREuareSize = 20
+const SQUARE = 40
 const VACANT = 'white'
+const BORDER = '#43464B'
 
-function drawSQUAREuare (x, y, color) {
-  ctx.fillStyle = color
-  ctx.fillRect(x*SQUARE, y*SQUARE, SQUARE, SQUARE)
-  ctx.strokeStyle = 'black'
-  ctx.strokeRect(x*SQUARE, y*SQUARE, SQUARE, SQUARE)
-}
+const canvas = init(BOARD_ROWS, BOARD_COLUMNS, SQUARE)
+const scoreElement = document.querySelector('#score')
+const gameoverElement = document.querySelector('#gameover')
+const newGameButton = gameoverElement.querySelector('button')
+const ctx = canvas.getContext('2d')
 
-let score = 0
-let board = []
+document.addEventListener('keydown', handleInput)
+newGameButton.addEventListener('click', handleNewGame)
 
-for (row = 0; row < BOARD_ROWS; row++) {
-  board[row] = []
-  for (col = 0; col < BOARD_COLUMNS; col++) {
-    board[row][col] = VACANT
+function handleInput (event) {
+  if (event.keyCode == 37) {
+    p.moveLeft()
+    dropStart = Date.now()
+  } else if (event.keyCode == 38) {
+    p.rotate()
+    dropStart = Date.now()
+  } else if (event.keyCode == 39) {
+    p.moveRight()
+    dropStart = Date.now()
+  } else if (event.keyCode == 40) {
+    p.moveDown()
   }
 }
 
-function drawBoard () {
-  for (row = 0; row < BOARD_ROWS; row++) {
-    for (col = 0; col < BOARD_COLUMNS; col++) {
-      drawSQUAREuare(col, row, board[row][col])
-    }
-  }
+function handleNewGame () {
+  score = 0
+  gameOver = false
+  emptyBoard()
+  drawBoard()
+  p = randomPiece()
+  scoreElement.innerHTML = score
+  gameoverElement.classList.add('hide')
 }
-
-drawBoard()
 
 const pieces = [
   [Z, '#73956F'],
@@ -43,13 +47,51 @@ const pieces = [
   [I, '#D7BE82'],
   [J, '#FE7F2D']
 ]
+let p = randomPiece()
+
+let score = 0
+let board = []
+emptyBoard()
+
+function emptyBoard () {
+  for (row = 0; row < BOARD_ROWS; row++) {
+    board[row] = []
+    for (col = 0; col < BOARD_COLUMNS; col++) {
+      board[row][col] = VACANT
+    }
+  }
+}
+
+function init (rows, cols, square) {
+  const canvas = document.querySelector('#tetris')
+
+  canvas.width = cols * square
+  canvas.height = rows * square
+
+  return canvas
+}
+
+function drawSquare (x, y, color) {
+  ctx.fillStyle = color
+  ctx.fillRect(x*SQUARE, y*SQUARE, SQUARE, SQUARE)
+  ctx.strokeStyle = BORDER
+  ctx.strokeRect(x*SQUARE, y*SQUARE, SQUARE, SQUARE)
+}
+
+function drawBoard () {
+  for (row = 0; row < BOARD_ROWS; row++) {
+    for (col = 0; col < BOARD_COLUMNS; col++) {
+      drawSquare(col, row, board[row][col])
+    }
+  }
+}
+
+drawBoard()
 
 function randomPiece () {
   let random = Math.floor(Math.random() * pieces.length)
   return new Piece(pieces[random][0], pieces[random][1]) 
 }
-
-let p = randomPiece()
 
 function Piece (tetromino, color) {
   this.tetromino = tetromino
@@ -66,7 +108,7 @@ Piece.prototype.fill = function (color) {
   for (row = 0; row < this.activeTetromino.length; row++) {
     for (col = 0; col < this.activeTetromino.length; col++) {
       if (this.activeTetromino[row][col]) {
-        drawSQUAREuare(this.x + col, this.y + row, color)
+        drawSquare(this.x + col, this.y + row, color)
       }
     }
   }
@@ -97,9 +139,6 @@ Piece.prototype.moveRight = function () {
     this.clear()
     this.x++
     this.draw()
-  } else {
-    // lock piece and make new piece
-    
   }
 }
 
@@ -108,8 +147,6 @@ Piece.prototype.moveLeft = function () {
     this.clear()
     this.x--
     this.draw()
-  } else {
-    // lock piece and make new piece
   }
 }
 
@@ -143,7 +180,7 @@ Piece.prototype.lock = function () {
       }
 
       if (this.y + row < 0) {
-        alert('Game over')
+        gameoverElement.classList.remove('hide')
         gameOver = true
         break
       }
@@ -201,26 +238,8 @@ Piece.prototype.collision = function (x, y, piece) {
   return false
 }
 
-// user input
-document.addEventListener('keydown', handleInput)
-
-function handleInput (event) {
-  if (event.keyCode == 37) {
-    p.moveLeft()
-    dropStart = Date.now()
-  } else if (event.keyCode == 38) {
-    p.rotate()
-    dropStart = Date.now()
-  } else if (event.keyCode == 39) {
-    p.moveRight()
-    dropStart = Date.now()
-  } else if (event.keyCode == 40) {
-    p.moveDown()
-  }
-}
-
 let dropStart = Date.now()
-let gameOver = false
+let gameOver = true
 
 function drop () {
   let now = Date.now()
